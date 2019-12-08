f14665de1257cb746f33433847808ee50941073a
KuangPF <1633397286@qq.com>
2019-12-08 21:56:28 +0800

    feat: 添加基础组件

--- src/pages/base-component/index.tsx
+++ src/pages/base-component/index.tsx
@@ -1,16 +1,65 @@
 import Taro, { PureComponent } from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View, Button, Picker, Image } from '@tarojs/components'
 
-class BaseComponent extends PureComponent {
+interface IProps {}
+interface IStates {
+  timeValue: string
+  userInfo: {
+    avatarUrl: string
+    nickName: string
+  }
+}
+
+class BaseComponent extends PureComponent<IProps, IStates> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timeValue: '2019-12-08',
+      userInfo: {
+        avatarUrl: '',
+        nickName: ''
+      }
+    }
+  }
   handleGetUserInfo = e => {
+    const {
+      detail: { userInfo }
+    } = e
+    this.setState({
+      userInfo
+    })
+    console.log(e)
+  }
+
+  handleDateChange = e => {
+    const {
+      detail: { value }
+    } = e
+    Taro.showToast({
+      title: `已选中: ${value}`,
+      icon: 'none'
+    })
     console.log(e)
   }
   render() {
+    const { timeValue, userInfo } = this.state
     return (
-      <View>
-        <Button open-type="getUserInfo" lang="zh_CN" onGetUserInfo={this.handleGetUserInfo}>
+      <View className="page-content">
+        <Button className="mt15" open-type="getUserInfo" lang="zh_CN" onGetUserInfo={this.handleGetUserInfo}>
           获取用户信息
         </Button>
+        <Button type="primary" className="mt15" loading>
+          Loading...
+        </Button>
+        <Picker className="mt15" mode="date" value={timeValue} onChange={this.handleDateChange}>
+          <Button>时间选择器</Button>
+        </Picker>
+        {userInfo.avatarUrl && (
+          <View className="mt15">
+            <View>nickName: {userInfo.nickName}</View>
+            <Image style={{ width: '100px', height: '100px' }} src={userInfo.avatarUrl} />
+          </View>
+        )}
       </View>
     )
   }
