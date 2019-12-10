630392382be4a020b70aab43ee094521fba90311
KuangPF <1633397286@qq.com>
2019-12-11 01:54:35 +0800

    feat: dva demo done

--- src/models/data-dva/index.ts
+++ src/models/data-dva/index.ts
@@ -4,13 +4,37 @@ export interface IDvaState {
   count: number
 }
 
+export enum actionType {
+  Increase = 'Increase',
+  Decrease = 'Decrease'
+}
+
 const state: IDvaState = {
   count: 1
 }
 export default {
   namespace,
   state,
-  effects: {},
+  effects: {
+    *opeateDecrease({ payload }, { select, put }) {
+      try {
+        const { operateType } = payload
+        let { count } = yield select(state => state[namespace])
+        if (operateType === actionType.Decrease) {
+          count -= 1
+        } else if (operateType === actionType.Increase) {
+          count += 1
+        }
+
+        yield put({
+          type: 'setState',
+          payload: {
+            count
+          }
+        })
+      } catch (error) {}
+    }
+  },
   reducers: {
     setState(state, { payload }) {
       return { ...state, ...payload }
--- src/pages/data-dva/index.tsx
+++ src/pages/data-dva/index.tsx
@@ -4,9 +4,9 @@
  * @Description: 数据管理 dva
  */
 
-import Taro, { PureComponent } from '@tarojs/taro'
+import Taro, { PureComponent, Config } from '@tarojs/taro'
 
-import { namespace, IDvaState } from '@/models/data-dva/index'
+import { namespace, IDvaState, actionType } from '@/models/data-dva/index'
 import { connect } from '@tarojs/redux'
 import { View } from '@tarojs/components'
 
@@ -17,11 +17,54 @@ interface IProps extends IDvaState {
 }
 
 class DataDva extends PureComponent<IProps> {
+  config: Config = {
+    usingComponents: {
+      'van-button': '/components/vant-weapp/dist/button/index'
+    }
+  }
+  handleCountOperate(operateType) {
+    this.props.dispatch({
+      type: `${namespace}/opeateDecrease`,
+      payload: {
+        operateType
+      }
+    })
+  }
+  handleSetZero() {
+    this.props.dispatch({
+      type: `${namespace}/setState`,
+      payload: {
+        count: 0
+      }
+    })
+  }
   render() {
     const { count } = this.props
     return (
       <View className="page-content">
-        <View className='count blod'>Count: {count}</View>
+        <View className="count blod">Count: {count}</View>
+        <View className="operate">
+          <van-button
+            size="small"
+            onClick={() => {
+              this.handleCountOperate(actionType.Decrease)
+            }}
+          >
+            减少
+          </van-button>
+          <van-button
+            size="small"
+            class="ml15"
+            onClick={() => {
+              this.handleCountOperate(actionType.Increase)
+            }}
+          >
+            增加
+          </van-button>
+          <van-button size="small" class="ml15" type='primary' onClick={this.handleSetZero}>
+            置为0
+          </van-button>
+        </View>
       </View>
     )
   }
